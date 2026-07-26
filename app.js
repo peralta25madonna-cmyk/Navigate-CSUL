4796ca5f54985c621afdb160c86d4c3437146f56
peralta25madonna-cmyk <peralta25madonna@gmail.com>
2026-07-26 12:21:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+import * as ecs from '@8thwall/ecs'
+
 const onxrloaded = () => {
 
   console.log("XR Ready")
+
+  XR8.addCameraPipelineModule({
+
+    name: 'camera',
+
+    onStart: () => {
+      console.log("Camera Started")
+    },
+
+  })
 
 }
 
