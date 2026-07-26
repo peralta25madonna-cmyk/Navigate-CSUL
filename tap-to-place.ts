f0f4b0e84860ac373e3fe705d08a2fe06aaf3f3a
peralta25madonna-cmyk <peralta25madonna@gmail.com>
2026-07-26 13:14:56 +0800

    Update tap-to-place.ts

--- tap-to-place.ts
+++ tap-to-place.ts
@@ -4,25 +4,72 @@ const OBJECT_PLACED_EVENT = 'object-placed'
 
 ecs.registerComponent({
   name: 'tap-to-place',
+
   schema: {
     prefab: 'eid'
   },
+
   stateMachine: ({world, eid, schemaAttribute, defineState}) => {
-    defineState('initial').initial().listen(eid,  ecs.input.SCREEN_TOUCH_START, (e) => {
-      if (!e.data.worldPosition) {
-        return
-      }
-      const newEid = world.createEntity(schemaAttribute.get(eid).prefab)
-      const newEntity = world.getEntity(newEid)
-      newEntity.setLocalPosition(e.data.worldPosition)
-      newEntity.set(
-  ecs.Quaternion,
-  ecs.math.quat.yRadians(0)
-)
-      world.events.dispatch(eid, OBJECT_PLACED_EVENT)
-    })
+
+    defineState('initial')
+      .initial()
+      .listen(eid, ecs.input.SCREEN_TOUCH_START, (e) => {
+
+        console.log("SCREEN TOUCHED")
+
+        if (!e.data.worldPosition) {
+
+          console.log("NO WORLD POSITION")
+
+          return
+        }
+
+        console.log(
+          "WORLD POSITION:",
+          e.data.worldPosition
+        )
+
+
+        const prefab = schemaAttribute.get(eid).prefab
+
+
+        if (!prefab) {
+
+          console.log("NO PREFAB ASSIGNED")
+
+          return
+        }
+
+
+        const newEid = world.createEntity(prefab)
+
+        const newEntity = world.getEntity(newEid)
+
+
+        newEntity.setLocalPosition(
+          e.data.worldPosition
+        )
+
+
+        newEntity.set(
+          ecs.Quaternion,
+          ecs.math.quat.yRadians(0)
+        )
+
+
+        console.log("AR OBJECT CREATED")
+
+
+        world.events.dispatch(
+          eid,
+          OBJECT_PLACED_EVENT
+        )
+
+      })
+
   }
 })
+
 
 export {
   OBJECT_PLACED_EVENT,
